8cc5612fddf32c1732081e3066397a1e5bb08b2a
Scott Fanetti <scott.fanetti@gmail.com>
2022-10-31 16:51:50 -0400

    updated formatter

--- script.py
+++ script.py
@@ -24,7 +24,7 @@ def formatter(dates):
             return ""
     def fmt(x):
         dt = datetime.fromisoformat(get_dates(x))
-        if dt.hour == 0:
+        if dt.hour == 0 or x == 0 or x == len(dates):
             return dt.strftime("%b %d %I %p")
         else:
             return dt.strftime("%I %p")
